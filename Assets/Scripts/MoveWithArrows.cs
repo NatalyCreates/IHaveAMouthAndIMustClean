6c40d90ea73f1a13eee24fbc31556c1b86405583
NatalyCreates <nataly@natalycreates.com>
2016-01-29 11:49:58 +0200

    starting work on scene

---
+++ Assets/Scripts/MoveWithArrows.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveWithArrows : MonoBehaviour {
+
+    Vector2 direction = Vector2.zero;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            gameObject.transform.position += new Vector3(1,2,0);
+            direction = Vector2.right;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            direction = Vector2.left;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            direction = Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            direction = Vector2.down;
+        }
+    }
+}
