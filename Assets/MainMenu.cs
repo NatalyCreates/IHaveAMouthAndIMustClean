9d17e049a8e97966f5c9e69156d61d3ca1ff8dd1
Jonathan Beyt-Yacov <jonabeyt@gmail.com>
2016-01-30 13:01:51 +0200

    Menu and stuff

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	public void StopBrushing (){
+			Application.Quit ();
+	}
+
+	public void StartGame(){
+		Application.LoadLevel ("mouth");
+	}
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
